cee5d341b1577ea0f34530a75c77266e250b723c
T-Heier <trentheier@gmail.com>
2021-04-20 19:40:36 -0400

    wrote the first inquirer prompt and got it to connect with my manager class constructor.

--- index.js
+++ index.js
@@ -4,3 +4,62 @@ const Intern = require('./lib/Intern');
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
+
+
+const manQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the manager's name",
+        // validation
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's team id number?",
+        // validation
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email address?",
+        // validation
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+        // validation
+    },
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// App Start
+menu = () => {
+    createManager = () => {
+        inquirer.prompt(manQuestions).then(({name, id, email, officeNumber}) => {
+            const manager = new Manager(name, id, email, officeNumber)
+            console.log(manager);
+        })
+    }
+    createManager();
+}
+
+
+menu();
