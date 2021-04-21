df5fd26d74d01ae36550f8113ab1df387b2a4a34
T-Heier <trentheier@gmail.com>
2021-04-20 23:12:00 -0400

    added more prompts after you create manager

--- index.js
+++ index.js
@@ -34,6 +34,69 @@ const manQuestions = [
     
 ]
 
+const engQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the engineer's name",
+        // validation
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineers's id number?",
+        // validation
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?",
+        // validation
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's github username?"
+
+    }
+]
+
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "Enter the manager's name",
+        // validation
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's team id number?",
+        // validation
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email address?",
+        // validation
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school does this intern attend?"
+
+    }
+]
+
+const addTeamMember = [
+    {
+        type: "list",
+        name: "teamMemberList",
+        message: "Add a team member?",
+        choices: ["Engineer", "Intern", "No more team members."]
+    }
+]
+
 
 
 
@@ -56,9 +119,34 @@ menu = () => {
         inquirer.prompt(manQuestions).then(({name, id, email, officeNumber}) => {
             const manager = new Manager(name, id, email, officeNumber)
             console.log(manager);
+            newTeamMember();
         })
     }
+    createEngineer = () => {
+        inquirer.prompt(engQuestions).then(({name, id, email, github}) => {
+            const engineer = new Engineer(name, id, email, github);
+            console.log(engineer);
+        })
+    }
+
+    createIntern = () => {
+        inquirer.prompt(internQuestions).then(({name, id, email, school}) => {
+
+        })
+    }
+
+    newTeamMember = () => {
+        inquirer.prompt(addTeamMember)
+        if (addTeamMember == "Engineer") {
+            createEngineer();
+        } else if (addTeamMember == "Intern") {
+            createIntern();
+        }
+    }
+
+
     createManager();
+    
 }
 
 
