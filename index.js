3c76e1c8100d6ddc49ad51099d1ccbb3dadd22fc
T-Heier <trentheier@gmail.com>
2021-04-22 17:26:45 -0400

    finished the conditionals to create all the employees. starting on the create html function

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+const generateHTML = require('./dist/generateHTML')
 const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
+const { create } = require('domain');
 
 
 const manQuestions = [
@@ -65,19 +67,19 @@ const internQuestions = [
     {
         type: "input",
         name: "name",
-        message: "Enter the manager's name",
+        message: "Enter the interns's name",
         // validation
     },
     {
         type: "input",
         name: "id",
-        message: "What is the manager's team id number?",
+        message: "What is the interns's team id number?",
         // validation
     },
     {
         type: "input",
         name: "email",
-        message: "What is the manager's email address?",
+        message: "What is the intern's email address?",
         // validation
     },
     {
@@ -98,64 +100,65 @@ const addTeamMember = [
 ]
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+// this is the array of data where the employee's should get pushed into?
+const employees = [];
 
 
 
 // App Start
 menu = () => {
+    // creates a manager with the prompt of manager questions
     createManager = () => {
         inquirer.prompt(manQuestions).then(({name, id, email, officeNumber}) => {
             const manager = new Manager(name, id, email, officeNumber)
+            employees.push(manager)
             console.log(manager);
             newTeamMember();
         })
     }
+    // creates a new teammember with the engineer or intern prompts
     newTeamMember = () => {
-        inquirer.prompt(addTeamMember).then(() => {
-            
-            if (addTeamMember.teamMemberList == "Engineer") {
+        inquirer.prompt(addTeamMember)
+        .then(({teamMemberList}) => {
+            if (teamMemberList == "Engineer") {
                 createEngineer();
+            } else if (teamMemberList == "Intern") {
+                createIntern();
             } else {
-                if (addTeamMember.teamMemberList == "Intern") {
-                    createIntern();
-                }
+                teamMemberList == "No more team members"
+                // run generate html script
             }
         })
     }
+    // creates an engineer using the prompt questions
     createEngineer = () => {
         inquirer.prompt(engQuestions).then(({name, id, email, github}) => {
             const engineer = new Engineer(name, id, email, github);
             console.log(engineer);
+            employees.push(engineer)
+            newTeamMember();
         })
     }
-
+    // creates an intern using the prompt questions
     createIntern = () => {
         inquirer.prompt(internQuestions).then(({name, id, email, school}) => {
             const intern = new Intern(name, id, email, school);
             console.log(intern)
+            employees.push(intern)
+            newTeamMember();
         })
     }
-
-
-
-
     createManager();
+
 }
 
-    
-
-
-
 menu()
+
+
+function writeHTML () {
+    fs.writeFile(index.html, (generateHTML(employees)), (err) => {
+        if (err) {
+            console.error(err)
+        }
+    });
+}
